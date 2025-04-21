55d9e98f762755e3bb427967f3319bec371a707b
Bruno Candia <bruno.candia@endava.com>
2025-04-21 16:19:52 -0300

    Reactive form added and reusable component added

--- Client/src/app/register/register.component.ts
+++ Client/src/app/register/register.component.ts
@@ -1,24 +1,51 @@
-import { Component, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
+import { TextInputComponent } from "../forms/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent],
+  //imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   // usersFromHomeComponent = input.required<any>();
   cancelRegister = output<boolean>();
 
   model: any = {};
 
+  registerForm: FormGroup = new FormGroup({});
+
   constructor(
     private accountService: AccountService,
     private toastrService: ToastrService
   ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+matchValues(matchTo: string) {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { isMatching: true };
+    };
+  }
 
   register() {
     console.log(this.model);
