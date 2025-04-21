a6ae972fdc4166d176a4acd57fcf2bd1dd7c3a51
Bruno Candia <bruno.candia@endava.com>
2025-04-21 17:06:10 -0300

    DatePicker component added

--- Client/src/app/register/register.component.ts
+++ Client/src/app/register/register.component.ts
@@ -1,13 +1,14 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit, output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 import { TextInputComponent } from "../forms/text-input/text-input.component";
+import { DatePickerComponent } from "../forms/date-picker/date-picker.component";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent],
+  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent, DatePickerComponent],
   //imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
@@ -18,23 +19,41 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
+  maxDate: Date = new Date();
+
   registerForm: FormGroup = new FormGroup({});
 
   constructor(
     private accountService: AccountService,
-    private toastrService: ToastrService
+    private toastrService: ToastrService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    console.log(this.maxDate);
   }
 
   initializeForm() {
-    this.registerForm = new FormGroup({
-      userName: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    // Using FormBuilder to create the form group and form controls
+    this.registerForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+      gender: ['male'],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required]
     });
+
+    // Alternatively, you can use FormControl directly to create the form group and form controls
+    // this.registerForm = new FormGroup({
+    //   userName: new FormControl('', Validators.required),
+    //   password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    // });
 
     this.registerForm.controls['password'].valueChanges.subscribe(() => {
       this.registerForm.controls['confirmPassword'].updateValueAndValidity();
